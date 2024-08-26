f4e98d6f8c7f2527af3aaf94aabadee7272aef71
Sura-T <surafeltakele87@gmail.com>
2024-08-26 20:08:03 +0300

    arrangement of components file

---
+++ src/containers/index.js
@@ -0,0 +1,5 @@
+export {default as Blog}  from './blog/Blog';
+export {default as Features}  from './Features/features';
+export {default as Footer}  from './Footer/footer';
+export {default as Header}  from './Header/header';
+export {default as WhatGPT3}  from './WhatGPT3/whatGPT3';
