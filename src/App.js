373c646c45bc4baf935d6c1c6c0c11e1580dfa25
Sura-T <surafeltakele87@gmail.com>
2024-09-02 21:03:49 +0300

    navigation component added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import {Blog, Features, Footer, Header, Possibility, WhatGPT3} from './containers';
 import { Article, Brand, CTA, Feature, Navbar } from './components';
+import './App.css';
+import './index.css';
 
 function App() {
   return (
     <div className='App'>
       <div className='gradient__bg'>
-
+      <Navbar />
+      <Header />
       </div>
+      <Brand />
+      <WhatGPT3 />
+      <Features />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer />
     </div>
   )
 }
